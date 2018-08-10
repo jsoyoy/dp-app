f8ddf08c1f7fbe53c7063df7379e27743c3531fd
liuyang-fe <605564739@qq.com>
2018-08-10 19:38:09 +0800

    home: loadMore

--- app/page/Home/subpage/Favor.jsx
+++ app/page/Home/subpage/Favor.jsx
@@ -7,7 +7,7 @@ class Favor extends React.Component {
     super(props, context);
     this.state = {
       favoList: [],
-      isLoadMore: false,
+      isLoading: false,
       page: 0
     }
   }
@@ -16,35 +16,61 @@ class Favor extends React.Component {
       <div>
         <h3 className="home-list-title">猜你喜欢</h3>
         {
-          this.state.favoList.length > 0 ?
-            <HomeFavor favoList={this.state.favoList}/> :
-            <div>正在加载中。。。</div>
+          this.state.favoList.length > 0
+            ? <HomeFavor favoList={this.state.favoList}/>
+            : <div>正在加载中。。。</div>
         }
-
-        <div className="load-more">正在加载更多。。。</div>
+        <div className="load-more" ref="loadMore" style={{padding : "5px 0"}}>
+          {
+            <div onClick={this.loadMore.bind(this)}>
+              {
+                this.state.isLoading ? "正在加载更多..." : ""
+              }</div>
+          }
+        </div>
       </div>
     )
   }
   componentDidMount () {
     this.getFavorList()
-    this.loadMore()
+    let timer;
+    window.addEventListener('scroll', () =>{
+      if (timer) {
+        clearTimeout(timer)
+      }
+      // timer = setTimeout(() => {
+      let wrapper = this.refs.loadMore
+      let top = wrapper.getBoundingClientRect().top
+      let windowHeight = window.screen.height
+      console.log(timer, top, windowHeight);
+      if (top && top < windowHeight) {
+        this.loadMore()
+      }
+      // }, 500)
+    })
   }
   /*加载更多*/
   loadMore () {
+    let page = ++this.state.page;
     this.setState({
-      page: this.state.page++
+      page: page
     })
     this.getFavorList()
   }
   /*获取列表数据*/
   getFavorList () {
+    this.setState({
+      isLoading: true
+    })
+    console.log(this.state.isLoading);
     get(`/api/homelist/${encodeURIComponent(this.props.cityName)}/${this.state.page}`).then(res => {
       res.json().then(list => {
-        setTimeout(() => {
+        setTimeout(()=>{
           this.setState({
-            favoList: list.data
+            favoList: this.state.favoList.concat(list.data),
+            isLoading: this.state.page === 0 ? true : false
           })
-        }, 2000)
+        }, 1000)
       })
     })
   }
